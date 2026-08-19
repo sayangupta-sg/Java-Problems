7356bb1ff02d2eed3322ce3c935cbc5de64c7de5
Sayan Gupta <sayangupta4007@gmail.com>
2026-08-19 21:55:08 +0530

    Adding Question 9 and 10

---
+++ src/Question10.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Question10 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter string: ");
+        String s = input.next();
+
+        for (int i = 0; i < s.length() / 2; i++) {
+            if (s.charAt(i) != s.charAt(s.length() - i - 1)) {
+                System.out.println("Not Palindrome");
+                return;
+            }
+        }
+
+        System.out.println("Palindrome");
+    }
+}
