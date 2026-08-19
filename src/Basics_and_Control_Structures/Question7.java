009ae3d52a6bf8c298cf46242c9ae31c983839ef
Sayan Gupta <sayangupta4007@gmail.com>
2026-08-19 20:44:36 +0530

    Adding all

---
+++ src/Basics_and_Control_Structures/Question7.java
@@ -0,0 +1,30 @@
+package Basics_and_Control_Structures;
+
+import java.util.*;
+
+public class Question7 {
+
+    public static int factorial(int num) {
+
+        if (num <= 1) {
+            return 1;
+        }
+
+        return num * factorial (num - 1);
+
+    }
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number: ");
+        int n = scanner.nextInt();
+        scanner.close();
+
+        System.out.println("Fatorial of "+n+ " is "+factorial(n));
+
+
+    }
+
+}
